80b95aba686bf5df9211e0b053dba7fe4d472836
Dmytro Los <chicanodima@gmail.com>
2022-01-17 16:32:49 +0200

    stage 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-import { userName } from "../src/cli.js";
+import * as cliJS from '../src/cli.js';
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,7 @@
-import readlineSync from "readline-sync";
-console.log("Welcome to the Brain Games!");
-export var userName = readlineSync.question("May I have your name?");
-console.log("Hello " + userName + "!");
+/* eslint-disable no-var */
+/* eslint-disable import/no-mutable-exports */
+import readlineSync from 'readline-sync';
+// eslint-disable-next-line import/prefer-default-export
+export var userName = readlineSync.question('May I have your name?');
+console.log('Welcome to the Brain Games!');
+console.log(`Hello ${userName}!`);
