68b175e4a281d1c647ba01d21540c910148a8d8f
Dmytro Los <chicanodima@gmail.com>
2022-01-17 18:46:51 +0200

    fix bugs

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
+
 export default function greetings() {
-  console.log("Welcome to the Brain Games!");
-  var userName = readlineSync.question("May I have your name?");
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
   console.log(`Hello ${userName}!`);
 }
