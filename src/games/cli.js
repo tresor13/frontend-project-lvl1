eac205bbfbb195d72441b1f92974e75d5c97c9c8
Dmytro Los <chicanodima@gmail.com>
2022-01-19 12:57:19 +0200

    stage 5

--- src/games/cli.js
+++ src/games/cli.js
@@ -1,28 +1,28 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function round() {
-  let num = Math.floor(Math.random() * 100) + 1;
+  const num = Math.floor(Math.random() * 100) + 1;
   const userAnswer = readlineSync.question(`Question: ${num} \nYour answer: `);
   if (
-    (userAnswer === "yes" && num % 2 === 0) ||
-    (userAnswer === "no" && num % 2 === 1)
+    (userAnswer === 'yes' && num % 2 === 0)
+    || (userAnswer === 'no' && num % 2 === 1)
   ) {
-    console.log("Correct!");
+    console.log('Correct!');
     return true;
-  } else if (userAnswer !== "yes" && num % 2 === 0) {
+  }
+  if (userAnswer !== 'yes' && num % 2 === 0) {
     console.log(
-      `'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'.`
+      `'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'.`,
     );
     return false;
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'no'.`);
-    return false;
   }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'no'.`);
+  return false;
 }
 
 export default function game() {
-  console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May I have your name?  ");
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?  ');
   console.log(`Hello ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let correctAnswer = 0;
@@ -33,7 +33,9 @@ export default function game() {
       break;
     }
   }
-  correctAnswer < 3
-    ? console.log("Try again!")
-    : console.log(`Congratulations, ${userName}!`);
+  if (correctAnswer < 3) {
+    console.log('Try again!');
+  } else {
+    console.log(`Congratulations, ${userName}!`);
+  }
 }
