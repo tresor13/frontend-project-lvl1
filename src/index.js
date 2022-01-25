02531bb70843b1915b5b43f2b1826c250b5b59a1
Dmytro Los <chicanodima@gmail.com>
2022-01-25 15:14:25 +0200

    corrections for project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 export function greetings() {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log("Welcome to the Brain Games!");
+  const userName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${userName}!`);
   return userName;
 }
 
@@ -27,7 +27,7 @@ export function getScore(gameFunc, userName) {
     }
   }
   if (score < 3) {
-    console.log('Try again!');
+    console.log(`Let's try again, ${userName}`);
   } else {
     console.log(`Congratulations, ${userName}!`);
   }
